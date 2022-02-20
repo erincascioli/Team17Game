e977d81c4c85281c0045d0310b92192d8d4654d9
Donovan Scullion <irishvan1@gmail.com>
2022-02-20 14:43:19 -0500

    basic CollisionManager and Projectile classes added

---
+++ CrossBoa/CrossBoa/CollisionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CrossBoa
+{
+    public static class CollisionManager
+    {
+        private static Player player;
+        private static CrossBow crossbow;
+        private static Projectile projectile;
+
+    }
+}
---
+++ CrossBoa/CrossBoa/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CrossBoa
+{
+    public class Projectile : GameObject
+    {
+        public Projectile(Texture2D asset, Vector2 vector2, Point size) 
+            : base(asset, vector2, size)
+        {
+
+        }
+    }
+}
