f2c698a896db911817f8ae7f1cbef586474c604e
Ian Knecht <92757599+IanK9988@users.noreply.github.com>
2022-02-23 20:40:26 -0500

    Made screen shake easier to use
    + Added properties to Camera class
    + Added helper method to Camera class
    + Comments

--- CrossBoa/CrossBoa/Camera.cs
+++ CrossBoa/CrossBoa/Camera.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework.Input;
 
 namespace CrossBoa
 {
+    /// <summary>
+    /// A class that represents the game's camera system
+    /// </summary>
     public static class Camera
     {
         private static Matrix matrix = Matrix.CreateTranslation(0, 0, 0);
@@ -16,38 +19,69 @@ namespace CrossBoa
         private static int screenShakeFramesLeft;
         private static float screenShakeMagnitude = 0.5f;
 
+        /// <summary>
+        /// A Matrix representation of the camera location
+        /// </summary>
         public static Matrix Matrix
         {
             get { return matrix; }
         }
+
+        /// <summary>
+        /// How violently the screen should shake
+        /// </summary>
+        public static float ScreenShakeMagnitude
+        {
+            get { return screenShakeMagnitude; }
+            set { screenShakeMagnitude = value; }
+        }
+
 
         public static void Update(KeyboardState kbState)
         {
             // Shake the screen if the player presses space
             if (kbState.IsKeyDown(Keys.Space))
             {
-                screenShakeFramesLeft = 20;
+                ShakeScreen(20);
             }
 
-            // Shakes screen. Shakes less as time passes
+            // Only update the camera if it's different from the previous frame
+            UpdateScreenShake();
+            
+            if (cameraX != prevCameraX || cameraY != prevCameraY)
+            {
+                matrix = Matrix.CreateTranslation(cameraX, cameraY, 0);
+            }
+        }
+
+        /// <summary>
+        /// Shakes the screen
+        /// </summary>
+        /// <param name="frames">How many frames to shake the screen for</param>
+        public static void ShakeScreen(int frames)
+        {
+            screenShakeFramesLeft = frames;
+        }
+
+        /// <summary>
+        /// Automatically calculates and moves the camera based on remaining screen shake frames
+        /// </summary>
+        private static void UpdateScreenShake()
+        {
+            // Shakes screen if there are frames to shake for. Shakes less as time passes
             if (screenShakeFramesLeft > 0)
             {
                 screenShakeFramesLeft--;
 
-                cameraX = (int)Math.Round((2 * Program.RNG.NextDouble() - 1) * screenShakeMagnitude * screenShakeFramesLeft);
-                cameraY = (int)Math.Round((2 * Program.RNG.NextDouble() - 1) * screenShakeMagnitude * screenShakeFramesLeft);
+                cameraX = (int)Math.Round((2 * Program.RNG.NextDouble() - 1) * ScreenShakeMagnitude * screenShakeFramesLeft);
+                cameraY = (int)Math.Round((2 * Program.RNG.NextDouble() - 1) * ScreenShakeMagnitude * screenShakeFramesLeft);
             }
+
             // Reset screen position after shaking is finished
             else if (screenShakeFramesLeft == 0)
             {
                 matrix = Matrix.CreateTranslation(0, 0, 0);
                 screenShakeFramesLeft--;
-            }
-
-            // Only update the camera if it's different from the previous frame
-            if (cameraX != prevCameraX || cameraY != prevCameraY)
-            {
-                matrix = Matrix.CreateTranslation(cameraX, cameraY, 0);
             }
         }
     }
