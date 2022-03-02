ee39c009802d6c76a1d88db594113517cfceaa82
Ian Knecht (RIT Student) <ijk9988@rit.edu>
2022-03-01 20:43:56 -0500

    Improved arrow logic
    + Arrow pickup cooldown is now shorter
    + Arrow now spawns closer to the player
    + Arrow moves faster

--- CrossBoa/CrossBoa/CrossBow.cs
+++ CrossBoa/CrossBoa/CrossBow.cs
@@ -117,10 +117,10 @@ namespace CrossBoa
                 projectile.ChangeVelocity(
                     position,
                     Direction,
-                    300f);
+                    360f);
 
                 // Makes the projectile appear from the bow instead of behind the player.
-                projectile.Position += projectile.Velocity * 0.2f;
+                projectile.Position += projectile.Velocity * 0.07f;
             }
         }
            
