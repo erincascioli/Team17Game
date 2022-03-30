1a96cc4bf17d80ec27fad9fee8366bc45edc164a
Donovan Scullion <dps5393@rit.edu>
2022-03-29 21:13:49 -0400

    SpawnManager now completely handles the creation of slimes

--- CrossBoa/CrossBoa/Managers/SpawnManager.cs
+++ CrossBoa/CrossBoa/Managers/SpawnManager.cs
@@ -1,12 +1,46 @@
-﻿namespace CrossBoa.Managers
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using CrossBoa.Enemies;
+using CrossBoa.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CrossBoa.Managers
 {
     /// <summary>
     /// Manages enemy spawns and contains methods for spawning them
     /// </summary>
     public static class SpawnManager
     {
+        private static List<GameObject> gameObjectList;
+
+        public static List<GameObject> GameObjectList
+        {
+            set { gameObjectList = value; }
+        }
 
 
+        static SpawnManager()
+        {
+        }
 
+        
+        /// <summary>
+        /// Spawns a slime enemy
+        /// </summary>
+        /// <param name="position">The position to spawn the slime at</param>
+        public static void SpawnSlime(Point position)
+        {
+            Slime newSlime = new Slime(
+                Game1.slimeSpritesheet,
+                Game1.slimeDeathSpritesheet,
+                3,
+                new Rectangle(position, new Point(64, 64)));
+            CollisionManager.AddEnemy(newSlime);
+            gameObjectList.Add(newSlime);
+        }
     }
 }
