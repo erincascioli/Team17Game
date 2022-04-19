e24694d9d581c93220a81e2a9388f7cfc0c6552c
Donovan Scullion <irishvan1@gmail.com>
2022-04-19 16:05:48 -0400

    Identified but didn't fix parallaxing mistake in the background of the game over state Cleaned up parts of player stats to be more readable and reverted player invincibility back to what was agreed upon, after it got changed in a branch merge. I'm not sure the fps counter works correctly; I definitly dropped below 60 for a few seconds earlier and it didn't change.

--- CrossBoa/CrossBoa/Managers/PlayerStats.cs
+++ CrossBoa/CrossBoa/Managers/PlayerStats.cs
@@ -9,35 +9,19 @@ namespace CrossBoa.Managers
     /// </summary>
     public static class PlayerStats
     {
-        private static float playerInvulnerabilityTime = 3f;
-        private static float arrowVelocity = 400f;
-        private static float arrowDespawnTime = 20f;
+        /// <summary>
+        /// The speed that the player's arrow will fire at
+        /// </summary>
+        public static float PlayerInvulnerabilityTime { get; set; } = 1.5f;
 
         /// <summary>
         /// The speed that the player's arrow will fire at
         /// </summary>
-        public static float PlayerInvulnerabilityTime
-        {
-            get { return playerInvulnerabilityTime; }
-            set { playerInvulnerabilityTime = value; }
-        }
-
-        /// <summary>
-        /// The speed that the player's arrow will fire at
-        /// </summary>
-        public static float ArrowVelocity
-        {
-            get { return arrowVelocity; }
-            set { arrowVelocity = value; }
-        }
+        public static float ArrowVelocity { get; set; } = 400f;
 
         /// <summary>
         /// The time before the player arrow will return automatically
         /// </summary>
-        public static float ArrowDespawnTime
-        {
-            get { return arrowDespawnTime; }
-            set { arrowDespawnTime = value; }
-        }
+        public static float ArrowDespawnTime { get; set; } = 20f;
     }
 }
